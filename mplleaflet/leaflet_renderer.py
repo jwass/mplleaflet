af91c13c90efb5ca8f9c6b1e94f4788ed4e7b84c
Jacob Wasserman <jwasserman@gmail.com>
2014-05-04 10:15:03 -0400

    Finish SVG marker implementation.

--- mplleaflet/leaflet_renderer.py
+++ mplleaflet/leaflet_renderer.py
@@ -1,12 +1,13 @@
 from functools import partial
 
 from jinja2 import Template
+from mplexporter.renderers.base import Renderer
+import numpy as np
 
 from utils import iter_rings
 
-from mplexporter.renderers.base import Renderer
 
-svg_template = Template("""<svg width="400px" height="400px" viewBox="-200 -200 400 400" xmlns="http://www.w3.org/2000/svg" version="1.1">  <path d="{{ path }}" fill="red" stroke="blue" stroke-width="3" /> </svg>""")
+svg_template = Template("""<svg width="{{ width }}px" height="{{ height }}px" viewBox="-{{ width / 2 }} -{{ height / 2 }} {{ width }} {{ height }}" xmlns="http://www.w3.org/2000/svg" version="1.1">  <path d="{{ path }}" {% for k, v in style.iteritems() %}{{ k }}="{{ v }}" {% endfor %}/></svg>""")
 
 class LeafletRenderer(Renderer):
     def __init__(self, crs=None, epsg=None):
@@ -47,8 +48,25 @@ class LeafletRenderer(Renderer):
 
         return leaflet_style
 
+    def _convert_style_svg(self, style):
+        svg_style = {
+            'stroke': style['edgecolor'],
+            'stroke-width': style['edgewidth'],
+            'stroke-opacity': style['alpha'],
+        }
+        if style['facecolor'] != 'none':
+            svg_style['fill'] = style['facecolor']
+            svg_style['fill-opacity'] = style['alpha']
+
+        return svg_style
+
     def _svg_path(self, pathcodes, data):
-        """ Return the SVG path's 'd' element. """
+        """ 
+        Return the SVG path's 'd' element.
+    
+        Note that SVG's coordinate system is flipped about the 
+        y-axis.
+        """
         def gen_path_elements(pathcodes, data):
             counts = {'M': 1, 'L': 1, 'C': 3, 'Z': 0}
             it = iter(data)
@@ -57,25 +75,36 @@ class LeafletRenderer(Renderer):
                 for _ in range(counts[code]):
                     p = it.next()
                     yield str(p[0])
-                    yield str(p[1])
+                    yield str(-p[1])
 
         return ' '.join(gen_path_elements(pathcodes, data))
 
 
     def draw_path(self, data, coordinates, pathcodes, style,
                   offset=None, offset_coordinates="data", mplobj=None):
-        if coordinates == 'points':
+        properties = self._convert_style(style)
+        if coordinates == 'points' or coordinates == 'display':
             path_points = data
             if offset_coordinates != 'data':
                 pass  # Don't know how to work with this yet
             if self.transformfunc:
-                coords = self.transformfunc(offset)
+                coords = self.transformfunc(*offset)
             else:
                 coords = list(offset)
             geometry_type = 'Point'
 
+            # Find the size of the path, and increase 25%
+            size = np.ceil(1.25 * np.ptp(path_points, axis=0))
+            width = int(size[0])
+            height = int(size[1])
             path = self._svg_path(pathcodes, path_points)
-            properties = {'html': svg_template.render(path=path)}
+            svg_style = self._convert_style_svg(style)
+            properties = {'html': svg_template.render(path=path, 
+                                                      style=svg_style,
+                                                      width=width,
+                                                      height=height),
+                          'width': width,
+                          'height': height}
         else:
             if self.transformfunc:
                 data = [self.transformfunc(*c) for c in data]
@@ -90,7 +119,6 @@ class LeafletRenderer(Renderer):
             else:
                 geometry_type = 'LineString'
                 coords = rings[0]
-            properties = self._convert_style(style)
 
         feature = {
             "type": "Feature",
