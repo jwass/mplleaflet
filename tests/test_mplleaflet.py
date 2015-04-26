7a50da893c728eea32658e404caadc0bf56a8615
Jacob Wasserman <jwasserman@gmail.com>
2015-04-26 10:47:52 -0400

    Use fig_to_html for tests
    Remove accidental commit of vim .swp file

--- tests/test_mplleaflet.py
+++ tests/test_mplleaflet.py
@@ -3,9 +3,9 @@ import mplleaflet
 
 def test_basic():
     plt.plot([0, 0], [1, 1])
-    mplleaflet.fig_to_geojson()
+    mplleaflet.fig_to_html()
 
 
 def test_scatter():
     plt.scatter([0, 10, 0, 10], [0, 0, 10, 10], c=[1, 2, 3, 4])
-    mplleaflet.fig_to_geojson()
+    mplleaflet.fig_to_html()
