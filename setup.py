a490314f380177e64ddb36bbb2ee53542e052be6
Jacob Wasserman <jwasserman@gmail.com>
2015-04-26 10:38:08 -0400

    Fix up setup and requirements file
    Use own fork of mplexporter until the larger matplotlib/mplexporter
    discussions move forward.

--- setup.py
+++ setup.py
@@ -14,17 +14,22 @@ DOWNLOAD_URL = 'http://github.com/jwass/mplleaflet'
 LICENSE = 'BSD 3-clause'
 VERSION = '0.0.1'
 
-setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      url=DOWNLOAD_URL,
-      download_url=DOWNLOAD_URL,
-      license=LICENSE,
-      packages=find_packages(),
-      package_data={'': ['*.html']}, # Include the templates
-     )
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=DOWNLOAD_URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    packages=find_packages(),
+    package_data={'': ['*.html']}, # Include the templates
+    install_requires=[
+        "mplexporter",
+        "jinja2",
+    ],
+)
