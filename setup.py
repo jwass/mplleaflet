44555d66c8a256f75d0f1ecc5a3ba14e042d6d42
Sebastiano Milardo <s.milardo@hotmail.it>
2019-10-03 14:36:07 -0400

    Fixes import of git submodules

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
+import sys
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup, find_packages
+
+from _mplleaflet_setup import (require_clean_submodules, UpdateSubmodules)
 
 with open('AUTHORS.md') as f:
     authors = f.read()
@@ -17,6 +22,10 @@ DOWNLOAD_URL = 'http://github.com/jwass/mplleaflet'
 LICENSE = 'BSD 3-clause'
 VERSION = '0.0.5'
 
+# Make sure submodules are updated and synced
+root_dir = os.path.abspath(os.path.dirname(__file__))
+require_clean_submodules(root_dir, sys.argv)
+
 setup(
     name=NAME,
     version=VERSION,
@@ -29,10 +38,12 @@ setup(
     url=DOWNLOAD_URL,
     download_url=DOWNLOAD_URL,
     license=LICENSE,
-    packages=find_packages(),
+    cmdclass={'submodule': UpdateSubmodules},
+    packages=list(set(find_packages() + ["mplleaflet","mplleaflet/mplexporter","mplleaflet/mplexporter/renderers"])),
     package_data={'': ['*.html']}, # Include the templates
     install_requires=[
         "jinja2",
         "six",
+        "ipython",
     ],
 )
